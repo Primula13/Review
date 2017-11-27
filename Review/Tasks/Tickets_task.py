ee652474d10c36ce9b8cf6c27c613110fc65f84a
Primula13 <kornutaa@gmail.com>
2017-11-27 23:51:05 +0200

    Add files via upload

--- Review/Tasks/Tickets_task.py
+++ Review/Tasks/Tickets_task.py
@@ -1,10 +1,12 @@
 """Найти кол-во счастливых билетов в 1 серии."""
+
+
 total = 0
-for x in range(1000):
+for x in range(1, 14):
+    result = 0
     for y in range(1000):
-        first_half = x//100 + (x % 100)//10 + x % 10
-        second_half = y//100 + (y % 100)//10 + y % 10
-        if first_half == second_half:
-            total += 1
-total -= 1  # except equation 0 = 0
+        if y//100 + (y % 100)//10 + y % 10 == x:
+            result += 1
+    total += result**2
+total = total*2 + 1 #consider another half of ticket's number and include single equation 999 = 999
 print('Amount of happy tickets is %d' % total)
