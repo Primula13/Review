be16bdc66bb4ba39cd55e1f5caf05b7c1c55f917
Primula13 <kornutaa@gmail.com>
2017-11-28 00:03:14 +0200

    Add files via upload

--- Review/Tasks/Tickets_task.py
+++ Review/Tasks/Tickets_task.py
@@ -2,11 +2,10 @@
 
 
 total = 0
-for x in range(1, 14):
+for x in range(1, 27):
     result = 0
     for y in range(1000):
         if y//100 + (y % 100)//10 + y % 10 == x:
             result += 1
     total += result**2
-total = total*2 + 1  # consider another half of ticket's number and include single equation 999 = 999
 print('Amount of happy tickets is %d' % total)
